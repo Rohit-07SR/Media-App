f1de45f7c3e50bd9669ed0530af071fca7df8936
Rohit S <rohitishandsome07@gmail.com>
2025-05-29 16:32:18 +0530

    First app
    29-05-2025 4.32 pm

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>Hi, I am Rohit. Now i am learning react and javascript to learn new technologies and then learn node js later</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import EditPost from "./EditPost.js";
+import { DataProvider } from "./context/DataContext.js";
+
+function App() {
+  return (
+    <div className='App'>
+      <DataProvider>
+        <Header title="Rohit Media" />
+        <Nav />
+        <Routes>
+          <Route path="/" element={
+            <Home />} />
+          <Route path="post">
+            <Route index element={
+              <NewPost />} />
+            <Route path=":id" element={<PostPage />} />
+          </Route>
+          <Route path="/edit/:id" element={<EditPost />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer />
+      </DataProvider>
+    </div>
+  );
+}
+
+export default App;
